c1f5c73bd02096a9663dd87c214f69d5b875bfc7
Linzy2017 <578039491@qq.com>
2017-08-15 11:53:22 +0800

    引入字体图标，比如font-awesome的图标路径出错， 在webpack.base.conf.js里面修改limit要改大，把10000改为90000

--- vue-project/build/webpack.base.conf.js
+++ vue-project/build/webpack.base.conf.js
@@ -57,7 +57,7 @@ let webpackConfig = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 90000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
